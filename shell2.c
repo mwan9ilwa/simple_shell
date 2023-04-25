fbdec38a2ae590d06d0bf898f4d385cd97890c78
Keabetswe Kolobe <kkolobe2@gmail.com>
2023-04-25 06:24:19 -0700

    Fix

--- shell2.c
+++ shell2.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
-    char command[100];  // assuming maximum command length is 100 characters
-    
-    while (1) {  // infinite loop to keep prompting for commands
-        printf("$ ");  // print the prompt
-        fgets(command, sizeof(command), stdin);  // read the user's command
-        
-        printf("%s", command);  // print the user's command
-    }
-    
-    return 0;
+int main(void)
+{
+	char command[80];
+
+	while (1)
+	{
+		printf("mzkk$ ");
+		fgets(command, sizeof(command), stdin);
+
+		printf("%s", command);
+	}
+
+	return (0);
 }
