20ac01016d767505240b4d20cb5a59157b8f10bf
Keabetswe Kolobe <kkolobe2@gmail.com>
2023-04-25 02:41:54 -0700

    Betty

--- shell.c
+++ shell.c
@@ -1,40 +1,47 @@
 #include shell.h
 
-int main()
+int main(void)
 {
 	char command[MAX_LINE];
 
 	while (1)
 	{
 		char cwd[MAX_LINE];
+
 		if (getcwd(cwd, sizeof(cwd)) == NULL)
 		{
 			perror("error");
 			exit(1);
 		}
+
 		printf("#mzkk$ ");
 		fflush(stdout);
+
 		if (fgets(command, MAX_LINE, stdin) == NULL)
 		{	printf("\n");
 			exit(0);
 		}
-		if (command[strlen(command)-1] == '\n')
+		if (command[strlen(command) - 1] == '\n')
 		{
-			command[strlen(command)-1] = '\0';
+			command[strlen(command) - 1] = '\0';
 		}
+
 		char *args[MAX_LINE];
 		int i = 0;
-		char *token = strtok(command," ");
-		while (token  != NULL)
+		char *token = strtok(command, " ");
+
+		while (token != NULL)
 		{
 			args[i] = token;
 			token = strtok(NULL, " ");
 			i++;
 		}
+
 		args[i] = NULL;
 		pid_t pid = fork();
+
 		if (pid == 0)
-		{	
+		{
 			if execvp(args[0], args == -1)
 			{
 				perror("error");
@@ -42,16 +49,16 @@ int main()
 			}
 			exit(0);
 		}
-		else if (pid > 0 )
+		else if (pid > 0)
 		{
 			wait(&status);
 		}
-		else 
+		else
 		{
 			printf("Failed to fork\n");
 			exit(1);
 		}
 	}
 
-	return 0;
+	return (0);
 }
