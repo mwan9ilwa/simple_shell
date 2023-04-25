52c7e796fe015768ca34f8272f6e6a745830524c
kkolobe <keakrypt@gmail.com>
2023-04-25 23:00:21 +0200

    updated

--- shell1.c
+++ shell1.c
@@ -40,23 +40,41 @@ int exec_ls(void)
  */
 int process_command(char *command)
 {
-	if (strcmp(command, "/bin/ls\n") == 0)
-	{
-		return (exec_ls());
-	}
-	else if (strcmp(command, "exit\n") == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		char *program = strtok(command, " \n");
-		char *args[] = {program, NULL};
-		execvp(args[0], args);
+    command[strcspn(command, "\n")] = '\0';
 
-		perror("execvp");
-		return (-1);
-	}
+    char *program = strtok(command, " ");
+    char *args[80];
+    args[0] = program;
+    int i = 1;
+    while ((args[i] = strtok(NULL, " ")) != NULL) {
+        i++;
+    }
+    args[i] = NULL;
+
+    if (strcmp(program, "/bin/ls") == 0) {
+        return execvp(args[0], args);
+    }
+    else if (strcmp(program, "exit") == 0) {
+        return 0;
+    }
+    else {
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            execvp(args[0], args);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0) {
+            perror("fork");
+            return -1;
+        }
+        else {
+            wait(NULL);
+        }
+    }
+
+    return 1;
 }
 
 /**
